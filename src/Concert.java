91088ccf975402590e3e769b11c0442a34ed84b7
nerenererr <nerenererr@gmail.com>
2026-02-05 00:57:35 +0100

    Concert class in progress

--- src/Concert.java
+++ src/Concert.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Concert {
     private String artist;
@@ -68,4 +69,31 @@ public class Concert {
         this.active = active;
     }
 
+    public double calculateRevenue() {
+        double revenue = 0; //Variable para acumular el total
+        for (Ticket ticket : ticketsSold) {  //Recorre todas las entradas vendidas
+            revenue += ticket.getTotalPrice(); //Por cada entrada suma su precio al total
+        }
+        return revenue; //Devuelve el total acumulado
+    }
+
+    public double calculateAveragePrice() {
+        if (ticketsSold.isEmpty()) { //Si no hay ninguna entrada vendida devuelve 0
+            return 0;
+        }
+        return calculateRevenue() / ticketsSold.size(); //Se divide el total recaudado entre el tamaño del array de entradas vendidas
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Concert concert = (Concert) o;
+        return Double.compare(basePrice, concert.basePrice) == 0 && maxAforum == concert.maxAforum && active == concert.active && Objects.equals(artist, concert.artist) && Objects.equals(city, concert.city) && Objects.equals(ticketsSold, concert.ticketsSold);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(artist, city, basePrice, maxAforum, ticketsSold, active);
+    }
 }
